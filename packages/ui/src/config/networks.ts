6180141ecd50be216a21d84cf939298ed4015b1e
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-12-25 08:53:24 +0100

    Started importing components and hooks from the Sui dApp Starter.

---
+++ packages/ui/src/config/networks.ts
@@ -0,0 +1,8 @@
+export const LOCALNET_EXPLORER_URL = 'http://localhost:9001'
+export const DEVNET_EXPLORER_URL = 'https://devnet.suivision.xyz'
+export const TESTNET_EXPLORER_URL = 'https://testnet.suivision.xyz'
+export const MAINNET_EXPLORER_URL = 'https://suivision.xyz'
+
+export const EXPLORER_URL_VARIABLE_NAME = 'explorerUrl'
+
+export const NETWORKS_WITH_FAUCET = ['localnet', 'devnet', 'testnet']
